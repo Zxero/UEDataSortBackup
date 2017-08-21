aa52a5135a4f92bd59034d857d9cd8e7ac904bdf
jason <jphillips@cylindertestingsolutions.com>
2017-08-21 16:04:13 -0600

    Base level of code. Currently writes directories into console. Next steps are to split and then create folders for placement.

--- UEDataSortBackup/Program.cs
+++ UEDataSortBackup/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
-namespace UEDataSortBackup
+namespace UEDataSortBackup //goal: sort results folder into Year>Month>Day folders. Then back it up to a specified location. Don't do this bad.
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string sortPath = @"C:\UE1 Data\Results";
+                List<string> toSort = new List<string>(Directory.EnumerateDirectories(sortPath, "*_*"));
+
+                foreach (string path in toSort)
+                {
+                    Console.WriteLine("{0}", path.Substring(path.LastIndexOf("C") + 1));
+                }
+                Console.WriteLine("{0} directories found.", toSort.Count);
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Console.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Console.WriteLine(PathEx.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
