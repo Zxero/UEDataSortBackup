e109422bc207e813cf197ab02738ef9f0020b550
jason <jphillips@cylindertestingsolutions.com>
2017-08-25 15:12:32 -0600

    Removed all of the console writes. Now it just runs - though a console window does appear.

--- UEDataSortBackup/Program.cs
+++ UEDataSortBackup/Program.cs
@@ -26,41 +26,25 @@ namespace UEDataSortBackup //goal: sort results folder into Year>Month>Day folde
                         + '\\' + result[5] + '\\' + result[6] + '\\' + result[7] + '\\' + result[4]
                         + '_' + result[5] + '-' + result[6] + '-' + result[7] + ' ' + result[8];
 
-                    Console.WriteLine(newPath);
-                    Console.ReadLine();
 
                     while (Directory.Exists(newPath)) {                                                
                         x = x + 1;
                         newPath = result[0] + '\\' + result[1] + ' ' + result[2] + '\\' + result[3]
                         + '\\' + result[5] + '\\' + result[6] + '\\' + result[7] + '\\' + result[4]
                         + '_' + result[5] + '-' + result[6] + '-' + result[7] + ' ' + result[8] + " DUPE" + x;
-                        Console.WriteLine("New path already exists. Trying dupe path " + x);
-                        Console.ReadLine();
                     }
-                    Console.WriteLine(newPath);
-                    Console.ReadLine();
                     Directory.CreateDirectory(newPath);
-                    Console.WriteLine("The directory was created successfully!");
-                    Console.ReadLine();
 
                     foreach (string dirPath in Directory.GetFiles(path, "*.*",
                         SearchOption.AllDirectories))
                         File.Move(dirPath, dirPath.Replace(path, newPath));
-                    Console.WriteLine("Directory moved from " + path + " to " + newPath);
-                    Console.ReadLine();
-                    Console.WriteLine("About to delete " + path);
-                    Console.ReadLine();
                     DirectoryInfo di = new DirectoryInfo(path);
                     di.Delete();
-                    Console.WriteLine(path + " deleted successfully!");
                 }
-                Console.WriteLine("Program will now exit!");
-                Console.ReadLine();
             }
             catch (Exception e)
             {
-                Console.WriteLine("The process failed: {0}", e.ToString());
-                Console.ReadLine();
+                Debug.WriteLine("The process failed: {0}", e.ToString());
                                 }
 
         }
