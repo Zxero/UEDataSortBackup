9e0a668708fd24618886237e349f959d167e0764
jason <jphillips@cylindertestingsolutions.com>
2017-08-25 14:43:52 -0600

    Sort is complete! Currently using Console Writes/Reads for test purposes.

--- UEDataSortBackup/Program.cs
+++ UEDataSortBackup/Program.cs
@@ -19,29 +19,50 @@ namespace UEDataSortBackup //goal: sort results folder into Year>Month>Day folde
 
                 foreach (string path in toSort)
                 {
+                    int x = 0;
                     string folderName = path;
-                    string[] result = path.Split('_');
-                    string dateTime = result[1];
-                    string[] resultTwo = dateTime.Split(' ');
-                    string date = resultTwo[0];
+                    string[] result = path.Split('_', ' ', '\\', '-');
+                    string newPath = result[0] + '\\' + result[1] + ' ' + result[2] + '\\' + result[3]
+                        + '\\' + result[5] + '\\' + result[6] + '\\' + result[7] + '\\' + result[4]
+                        + '_' + result[5] + '-' + result[6] + '-' + result[7] + ' ' + result[8];
 
-                    Console.WriteLine(dateTime);
-                    Console.WriteLine(date);
-                    Console.WriteLine(folderName);
+                    Console.WriteLine(newPath);
                     Console.ReadLine();
 
+                    while (Directory.Exists(newPath)) {                                                
+                        x = x + 1;
+                        newPath = result[0] + '\\' + result[1] + ' ' + result[2] + '\\' + result[3]
+                        + '\\' + result[5] + '\\' + result[6] + '\\' + result[7] + '\\' + result[4]
+                        + '_' + result[5] + '-' + result[6] + '-' + result[7] + ' ' + result[8] + " DUPE" + x;
+                        Console.WriteLine("New path already exists. Trying dupe path " + x);
+                        Console.ReadLine();
+                    }
+                    Console.WriteLine(newPath);
+                    Console.ReadLine();
+                    Directory.CreateDirectory(newPath);
+                    Console.WriteLine("The directory was created successfully!");
+                    Console.ReadLine();
+
+                    foreach (string dirPath in Directory.GetFiles(path, "*.*",
+                        SearchOption.AllDirectories))
+                        File.Move(dirPath, dirPath.Replace(path, newPath));
+                    Console.WriteLine("Directory moved from " + path + " to " + newPath);
+                    Console.ReadLine();
+                    Console.WriteLine("About to delete " + path);
+                    Console.ReadLine();
+                    DirectoryInfo di = new DirectoryInfo(path);
+                    di.Delete();
+                    Console.WriteLine(path + " deleted successfully!");
                 }
-                Console.WriteLine("{0} directories found.", toSort.Count);
+                Console.WriteLine("Program will now exit!");
+                Console.ReadLine();
             }
-            catch (UnauthorizedAccessException UAEx)
+            catch (Exception e)
             {
-                Console.WriteLine(UAEx.Message);
-            }
-            catch (PathTooLongException PathEx)
-            {
-                Console.WriteLine(PathEx.Message);
-            }
-            Console.ReadLine();
+                Console.WriteLine("The process failed: {0}", e.ToString());
+                Console.ReadLine();
+                                }
+
         }
     }
 }
